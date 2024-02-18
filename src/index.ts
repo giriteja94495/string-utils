8a743d686cb3a1abfc432e1706010c912bba5a4b
Giriteja, Bille <Bille.Giriteja@epsilon.com>
2024-02-18 11:44:32 +0530

    string utils basic functions

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export function reverseString(str: string): string {
+    return str.split('').reverse().join('');
+  }
+  
+export function capitalizeString(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+  
+
+  
